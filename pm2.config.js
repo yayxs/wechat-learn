652ae9288430393a05b9ce96be5288e52cd889da
yayxs <leaderywl@163.com>
2021-06-15 14:04:44 +0800

    pm2 config

---
+++ pm2.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: 'wechat-demo', // 启动进程名
+      script: './src/main.js', // 启动文件
+      instances: 2, // 启动进程数
+      exec_mode: 'cluster', // 多进程多实例
+    },
+  ],
+}
