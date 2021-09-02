3c35a24d22248e49f657bca8b314bbb07fa938e9
yayxs <leaderywl@163.com>
2021-09-02 10:40:16 +0800

    fix(change pm2 config): change pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'wechat-demo', // 启动进程名
-      script: './src/main.js', // 启动文件
+      script: './src/app.js', // 启动文件
       instances: 2, // 启动进程数
       exec_mode: 'cluster', // 多进程多实例
     },
